667cc2a280519f5544ca26d40a7134432b482af2
theophile <theophile.rabut@edu.devinci.fr>
2023-01-09 18:37:35 +0100

    modif index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Requires
+
+const express = require("express");
+
+
+// Variables
+
+const PORT = process.env.PORT || 3000; // Default: 3000 (use the process.env.PORT else use 3000 by default)
+const app = express();
+
+
+// Listener
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(
+        "Server is listening on port "
+        + PORT + "\n" +
+        "Server URL: http://localhost:" + PORT
+    );
+});
