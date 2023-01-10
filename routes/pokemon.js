ca1492296775351d62c7f44c7816cc4fd4693d0f
anismch <95354292+anismch@users.noreply.github.com>
2023-01-10 23:49:46 +0100

    Update pokemon.js

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
-const { Pokemon }  = require("../modeles");
+const { Pokemon } = require("../modeles");
 const router = new Router();
 
-// Get collection types
+// Get collection pokemons
 router.get(
     "/pokemons",
     async (req, res) => {
@@ -12,5 +12,61 @@ router.get(
         res.json(pokemons);
     }
 );
+
+// Get pokemon by id
+router.get("/pokemons/:id", async (req, res) => {
+    const pokemon = await Pokemon.findByPk(parseInt(req.params.id));
+
+    if (pokemon) {
+        res.json(pokemon);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+// Create pokemon
+router.post("/pokemons", async (req, res, next) => {
+    try {
+        const pokemon = new Pokemon(req.body);
+        await pokemon.save({ fields: ['nom', 'type'] });
+        res.status(201).json(pokemon);
+    } catch (err) {
+        next(err);
+    }
+})
+
+// Update pokemon
+router.put("/pokemons/:id", async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        const [nbUpdated] = await Pokemon.update(req.body, {
+            where: {
+                id,
+            },
+        });
+
+        if(!nbUpdated) {
+            res.sendStatus(404);
+        } else {
+            res.json(await Pokemon.findByPk(id));
+        }
+    } catch (err) {
+        next(err);
+    }
+})
+
+// Delete pokemon
+router.delete("/pokemons/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const nbDeleted = await Pokemon.destroy({
+        where: {
+            id,
+        },
+    });
+
+    res.sendStatus(!nbDeleted ? 404 : 204);
+})
 
 module.exports = router;
