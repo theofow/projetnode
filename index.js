adcaa6191fcedaf434d553f03d8a7335ec690c4b
anetanya <anais.mfaboum_mbiafu@edu.devinci.fr>
2023-01-10 23:36:31 +0100

    ajout route regions pokemon index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const userrouter=require("./routes/user");
 const typesrouter = require("./routes/types");
+const regionrouter = require("./routes/region");
+const pokemonrouter = require("./routes/pokemon");
 
 // Variables
 const PORT = process.env.PORT || 3000; // Default: 3000
@@ -16,6 +18,8 @@ app.use(express.json());
 
 app.use(userrouter);
 app.use(typesrouter);
+app.use(regionrouter);
+app.use(pokemonrouter);
 
 app.listen(PORT, () => {
     console.log("Server is listening on port " + PORT);
