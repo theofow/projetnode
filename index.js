276e722672857211ba28e0301d905fdae8110a40
theophile <theophile.rabut@edu.devinci.fr>
2023-01-10 23:01:41 +0100

    edit index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Requires
-
 const express = require("express");
 const app = express();
 const userrouter=require("./routes/user");
-// Variables
 
-const PORT = process.env.PORT || 3000; // Default: 3000 (use the process.env.PORT else use 3000 by default)
+// Variables
+const PORT = process.env.PORT || 3000; // Default: 3000
 
 app.get("/", (req, res) => {
     res.json("I play Pokemon go ! ");
@@ -17,11 +16,5 @@ app.use(express.json());
 app.use(userrouter);
 
 app.listen(PORT, () => {
-    console.log(
-        "Server is listening on port "
-        + PORT + "\n" +
-        "Server URL: http://localhost:" + PORT
-    );
+    console.log("Server is listening on port " + PORT);
 });
-
-// Routes
